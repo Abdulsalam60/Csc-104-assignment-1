866b075030f02b35e0d40433aa23f20b9a8d0777
Abdulsalam60 <81860822+Abdulsalam60@users.noreply.github.com>
2021-04-03 20:12:28 +0100

    Add files via upload

---
+++ Nasirjnr.py
@@ -0,0 +1,33 @@
+# 1. Write a short python code to dispay the name of your name & department
+name = 'Abdulsalam Aiyu'
+department = 'Computer Science'
+print('Name:',name,"\n",'Department:',department )
+# 2.Assign the string "Boo!" to the variable scare and print the variable
+scare = "Boo!"
+print(scare)
+# 3. Add two varible and assign the result to the third variable. print all the variable name
+cool1 = 15
+cool2 = 5
+cool3 = cool1 + cool2
+print(cool1, cool2, cool3)
+# 4.Divide 12 by the decimal .5 and assign the result to the variable outcome and print the variable
+outcome = 12 / .5
+print(outcome)
+# 5. Assign the sum of two numbers to a variable and print the variable
+sum_of_two_numbers = 10 + 10
+print(sum_of_two_numbers)
+# 6.Write the code that finds the reminder when 8 is divided by 3
+smell = 8 % 3
+print(smell) 
+# 7. Rewrite the following statement to force this order: First, multiply 2 by 4. Then add 4 and 2. Then multiply the first result by the second result. Print the result 
+x = 2*4*4+2
+smell_1 = 2*4
+smell_2 = 4+2
+result = smell_1*smell_2 
+print(result)
+
+
+
+
+
+
